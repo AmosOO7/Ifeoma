456035ae6a5a9604096114964770189365774290
AmosOO7 <ayokunene@gmail.com>
2024-11-21 16:39:32 +0100

    more design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,10 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div
+        className="whiteBox"
+        style={{ color: "pink", marginLeft: "60vw" }}
+      ></div>
       <ul>
         <li className="siteName">
           <Link to="/">Home</Link>
