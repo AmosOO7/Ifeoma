ea810abeb56c0db6825d97740a982978b6ac9979
AmosOO7 <ayokunene@gmail.com>
2024-11-21 16:15:33 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Navbar />
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
